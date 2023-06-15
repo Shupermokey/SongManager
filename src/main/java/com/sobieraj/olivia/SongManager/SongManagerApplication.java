11509ead5d29bff88be642e38df405a3f7224484
Shupermokey <a.e.sobieraj@gmail.com>
2023-06-15 15:59:22 -0500

    Initial project

---
+++ src/main/java/com/sobieraj/olivia/SongManager/SongManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sobieraj.olivia.SongManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobieraj/olivia/SongManager/SongManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobieraj.olivia.SongManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
